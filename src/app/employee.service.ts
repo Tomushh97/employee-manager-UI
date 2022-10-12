af6b337e9414a2d0ad189953e98b4344dda38077
Tomasz <tomasz.golec.97@gmail.com>
2022-10-12 13:07:02 +0200

    updated request handlers for update employee and delete employee

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -15,6 +15,20 @@ export class EmployeeService {
     return this.http.get<Employee[]>(`${this.apiServiceUrl}/employee/all`);
   }
   public addEmployee(employee: Employee): Observable<Employee> {
-    return this.http.post<Employee>(`${this.apiServiceUrl}/employee/add`, employee); //tutaj podajemy w employee to co chcemy z frontu na back wyslacs
+    return this.http.post<Employee>(
+      `${this.apiServiceUrl}/employee/add`,
+      employee
+    ); //tutaj podajemy w employee to co chcemy z frontu na back wyslacs
+  }
+  public updateEmployee(employee: Employee): Observable<Employee> {
+    return this.http.put<Employee>(
+      `${this.apiServiceUrl}/employee/update`,
+      employee
+    );
+  }
+  public deleteEmployee(employeeId: number): Observable<void> {
+    return this.http.delete<void>(
+      `${this.apiServiceUrl}/employee/delete/${employeeId}`
+    );
   }
 }
